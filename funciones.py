3234cea03206c73f5be247c9b83e5a6740eade2b
piero-mz <pamezamarin@gmail.com>
2025-04-12 19:22:59 -0500

    Prueba02

--- funciones.py
+++ funciones.py
@@ -1,8 +1,12 @@
 def hola1():
     print("hola")
+    print("aaa")
 
 def hola2():
     print("hol2")
+    print("aaax2")
+    print("aaax3")
 
 def hola3():
     print("hola3")
+    print("aaaaaaaa")
