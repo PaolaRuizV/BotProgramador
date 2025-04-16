5fa2830ef47a03da4587b658c4d33f7cbb505f4d
PaolaRuizV <paolarosmery11@gmail.com>
2025-04-16 17:59:29 -0500

    Funciones

--- funciones.py
+++ funciones.py
@@ -5,3 +5,32 @@ def hola1():
 def hola3():
     print("hola3")
     print("aaaaaaaa")
+
+import subprocess
+import pyautogui
+import time
+
+def abrir_notepad():
+    subprocess.Popen(['notepad.exe'])
+    time.sleep(1)  # Espera a que se abra
+    pyautogui.write('Hola :D', interval=0.1)
+
+def abrir_paint():
+    subprocess.Popen(['mspaint.exe'])
+    time.sleep(3)  # Espera a que Paint esté listo
+
+    pyautogui.moveTo(400, 400)
+    pyautogui.dragRel(200, 0, duration=0.5)   # derecha
+    pyautogui.dragRel(0, 200, duration=0.5)   # abajo
+    pyautogui.dragRel(-200, 0, duration=0.5)  # izquierda
+    pyautogui.dragRel(0, -200, duration=0.5)  # arriba
+
+def tomar_captura():
+    screenshot = pyautogui.screenshot()
+    screenshot.save('captura_pantalla.png')
+    print("¡Captura tomada y guardada como 'captura_pantalla.png'!")
+
+
+# abrir_notepad()
+# abrir_paint()
+# tomar_captura()
